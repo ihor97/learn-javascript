5e13833c77365e4b2413b7e9783ee682aeac0c29
ihor97 <daniel.waffner@gmail.com>
2023-07-01 18:43:46 +0100

    Надсилання форми з файлом

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-// За допомогою наведених нижче методів ми можемо змінювати поля в об’єкті FormData:
-
-// formData.append(name, value) – додає до об’єкта поле з іменем name і значенням value,
-// formData.append(name, blob, fileName) – додає поле так, ніби це <input type="file">, третій аргумент fileName встановлює ім’я файлу (не ім’я поля форми), ніби це ім’я з файлової системи користувача,
-// formData.delete(name) – видаляє поле по заданому name,
-// formData.get(name) – дістає значення поля по заданому name,
-// formData.has(name) – перевіряє чи існує поле по заданому name, повертає true, інакше false
-
-// Ще існує метод set, його синтаксис такий самий, як у append. Різниця в тому, що 
-// .set видаляє всі наявні поля з ім’ям name і тільки потім додає нове. Тобто цей метод гарантує, 
-// що існуватиме лише одне поле з ім’ям name, у всьому іншому він аналогічний .append:
+// Об’єкти FormData завжди посилаються із заголовком Content-Type: multipart/form-data, 
+// цей спосіб кодування дозволяє надсилати файли. Таким чином, 
+// поля <input type="file"> теж відправляються, як і при використанні разі звичайної форми.
 
 
-let formData = new FormData();
-formData.append('key1', 'value1');
-formData.append('key2', 'value2');
+let formElem=document.getElementById('formElem')
 
-// Список пар ключ/значення
-for(let [name, value] of formData) {
-  alert(`${name} = ${value}`); // key1 = value1, then key2 = value2
-}
+formElem.onsubmit = async (e) => {
+  e.preventDefault();
 
+  let response = await fetch('/article/formdata/post/user-avatar', {
+    method: 'POST',
+    body: new FormData(formElem)
+  });
 
+  let result = await response.json();
+
+  alert(result.message);
+};
