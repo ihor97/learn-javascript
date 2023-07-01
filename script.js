25520007463275939f16cc99aad27a8b4738a527
ihor97 <daniel.waffner@gmail.com>
2023-07-01 18:46:19 +0100

    Надсилання форми з даними Blob

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-// Об’єкти FormData завжди посилаються із заголовком Content-Type: multipart/form-data, 
-// цей спосіб кодування дозволяє надсилати файли. Таким чином, 
-// поля <input type="file"> теж відправляються, як і при використанні разі звичайної форми.
+// Раніше у главі Fetch ми бачили, що дуже легко відправити динамічно згенеровані бінарні 
+// дані у форматі Blob. Ми можемо явно передати їх до параметра body запиту fetch.
 
+// Але на практиці буває зручніше відправляти зображення не окремо, а у складі форми, 
+// додавши додаткові поля для імені та інші метадані.
 
-let formElem=document.getElementById('formElem')
+// Крім того, сервери часто налаштовані на приймання саме форм, а не просто бінарних даних.
 
-formElem.onsubmit = async (e) => {
-  e.preventDefault();
-
-  let response = await fetch('/article/formdata/post/user-avatar', {
-    method: 'POST',
-    body: new FormData(formElem)
-  });
-
-  let result = await response.json();
-
-  alert(result.message);
+canvasElem.onmousemove = function(e) {
+  let ctx = canvasElem.getContext('2d');
+  ctx.lineTo(e.clientX, e.clientY);
+  ctx.stroke();
 };
+
+async function submit() {
+  let imageBlob = await new Promise(resolve => canvasElem.toBlob(resolve, 'image/png'));
+
+  let formData = new FormData();
+  formData.append("firstName", "John");
+  formData.append("image", imageBlob, "image.png");
+
+  let response = await fetch('/article/formdata/post/image-form', {
+    method: 'POST',
+    body: formData
+  });
+  let result = await response.json();
+  alert(result.message);
+}
